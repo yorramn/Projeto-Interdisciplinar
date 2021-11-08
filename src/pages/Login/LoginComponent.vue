f9f3efe2f08b87a42c5f303c501ff85cddba19b3
yorramn <bielyorramn@gmail.com>
2021-11-08 16:43:17 -0300

    melhoria da resposta e do alerta em cada

--- src/pages/Login/LoginComponent.vue
+++ src/pages/Login/LoginComponent.vue
@@ -4,8 +4,13 @@
     <div class="row">
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
         <div class="card border-0 shadow rounded-3 my-5">
+          <div class="row">
+            <div class="container">
+              <img :src="image" alt="" style="display: flex; justify-content: center;">
+            </div>
+          </div>
           <div class="card-body p-4 p-sm-5">
-            <h5 class="card-title text-center mb-5 fw-light fs-5">Entrar</h5>
+            <!-- <h5 class="card-title text-center mb-5 fw-light fs-5">Entrar</h5> -->
             <form>
               <div class="form-floating mb-3">
                 <input type="email" v-model="email" class="form-control" id="floatingInput" placeholder="name@example.com">
@@ -26,15 +31,15 @@
   </div>
 </div>
 </template>
-
 <script>
+import image from '../../assets/logo.png'
 import router from '../../router/index'
 const axios = require('axios')
 export default {
   name: 'LoginComponent',
   data () {
     return {
-
+      image: image
     }
   },
   methods: {
@@ -45,25 +50,24 @@ export default {
           password: senha
         })
         .then(function (response) {
-          const token = response.data.token
-          console.log(response)
-          localStorage.setItem('token', token)
-          localStorage.setItem('nome', response.data.user.name)
-          router.push({ name: 'Home' })
+          if (response.data.message != null) {
+            alert(response.data.message)
+          } else {
+            const token = response.data.token
+            localStorage.setItem('token', token)
+            localStorage.setItem('nome', response.data.user.name)
+            router.push({ name: 'Home' })
+          }
         })
-        // eslint-disable-next-line handle-callback-err
-        .catch(function (error) {
-          this.showMessage()
-        })
-    },
-    showMessage () {
-      this.$swal('Erro!', 'Login ou senha inválidos', 'error')
     }
   }
 
 }
 </script>
 <style scoped>
+body{
+  background-color: rgb(49, 48, 48);
+}
 .cent{
   display: flex;
   justify-content: center;
@@ -75,5 +79,9 @@ export default {
   font-size: 0.9rem;
   letter-spacing: 0.05rem;
   padding: 0.75rem 1rem;
+}
+img{
+  width: 100%;
+  height: 100%;
 }
 </style>
